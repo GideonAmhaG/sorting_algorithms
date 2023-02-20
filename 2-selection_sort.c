bcd6e5f535ef45e7554830b16be4e2bd4feb2453
GideonAmhaG <gideonamha@gmail.com>
2023-02-20 15:38:33 +0300

    added flag

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,7 +8,7 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int tmp = 0;
+	int tmp = 0, flag = 0;
 	size_t i, j, i_min = 0;
 
 	if (array == NULL || size == 0)
@@ -19,11 +19,18 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[i_min])
+			{
 				i_min = j;
+				flag = 1;
+			}
 		}
-		tmp = array[i];
-		array[i] = array[i_min];
-		array[i_min] = tmp;
-		print_array(array, size);
+		if (flag == 1)
+		{
+			tmp = array[i];
+			array[i] = array[i_min];
+			array[i_min] = tmp;
+			print_array(array, size);
+		}
+		flag = 0;
 	}
 }
